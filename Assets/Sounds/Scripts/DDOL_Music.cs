eb5df5a1edc7194cafed0848b1d220749ea6a239
TusharVaid30 <31384470+TusharVaid30@users.noreply.github.com>
2022-01-30 16:27:12 +0530

    Sound

--- Assets/Sounds/Scripts/DDOL_Music.cs
+++ Assets/Sounds/Scripts/DDOL_Music.cs
@@ -1,13 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class DDOL_Music : MonoBehaviour
+namespace Sounds.Scripts
 {
-    void Start()
+    public class DDOL_Music : MonoBehaviour
     {
-        DontDestroyOnLoad(gameObject);
-    }
-
+        private static DDOL_Music _instance ;
  
+        private void Awake()
+        {
+            //if we don't have an [_instance] set yet
+            if(!_instance)
+                _instance = this ;
+            //otherwise, if we do, kill this thing
+            else
+                Destroy(this.gameObject) ;
+ 
+ 
+            DontDestroyOnLoad(this.gameObject) ;
+        }
+    }
 }
